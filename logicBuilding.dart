bd10816194540da7c92a3a6065a02e2dd0ea60fc
muhammedsha@123 <muhammednk536@gmail.com>
2024-11-15 11:00:06 +0530

    first commiit

---
+++ logicBuilding.dart
@@ -0,0 +1,12 @@
+/*  Create the function that takes an object and returns the average of marks. (Score 2)
+ Examples
+ getAverage({name: "John", marks: [25, 45, 10, 30] }) ➞ 27.5
+ getAverage({name: "Kevin", marks: [50, 45, 40, 40, 25] }) ➞ 40 */
+
+void main() {
+  GetAvrage(String name ,List marks) {
+    int totel = marks.reduce((a, b) => a + b);
+    return totel / marks.length;
+  }
+  print(GetAvrage('muhammed',[25, 45, 10, 30]));
+}
